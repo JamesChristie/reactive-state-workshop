39714748ccef849ff8e2336129f978b07ca80d7d
James Christie <james.aaron.christie@gmail.com>
2017-12-13 12:06:31 -0600

    Update app title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('functional-reactive-app-root');
   }));
 
   it('should render title in a h1 tag', async(() => {
